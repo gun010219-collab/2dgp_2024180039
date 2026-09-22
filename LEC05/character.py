03c5d5c58005b3d2cd342a0f7e55ce74c5471722
gun010219-collab <gun010219@gmail.com>
2026-09-22 14:24:19 +0900

    개발 시잣,반복 루프 추가

--- LEC05/character.py
+++ LEC05/character.py
@@ -1,17 +1,17 @@
 from pico2d import *
 
 
-open_canvas(800, 600)
+open_canvas(1000, 1000)
 
-# 여기를 채우시오.
+character = load_image('character.png')
 
+character.draw(400, 300)
+character.draw(300, 200)
+character.draw(500, 400)
+for x in range(0, 9):
+    for y in range(0, 7):
+        character.draw(x * 100, y * 100)
 
-
-
-
-
-
+update_canvas()
 delay(2)
-
 close_canvas()
-
